98aa05af0f5b28b073b286416c48e942af174749
lorenzoc <lorenzoc@iastate.edu>
2019-11-21 13:18:41 -0600

    Added rebayes symbol to header and made header a link to website. Also fixed margin on top of page.

--- src/js/dataData.js
+++ src/js/dataData.js
@@ -2,6 +2,7 @@
 function getdataOptions(){
     //Gets value of input
     var filepath = document.getElementById("datafilepath").value;
+    //Removes file path from filename
     var filename = filepath.substring(filepath.lastIndexOf('\\')+1);
     //Checks to make sure input is not empty, else return the input as a script
     if(filename.length < 1){
