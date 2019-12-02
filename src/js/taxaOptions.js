882ebbcb2800407e659db67fb3e641f124f23fff
lorenzoc <lorenzoc@iastate.edu>
2019-12-02 11:41:47 -0600

    Created the taxa tab, need to work on adding datatable

---
+++ src/js/taxaOptions.js
@@ -0,0 +1,12 @@
+function createTaxaOptions(){
+    //Clears the taxa div
+    $("#taxaOptions").empty();
+
+    //Gets the taxa div
+    var option = document.getElementById("taxaOptions");
+
+    
+
+}
+
+createTaxaOptions()
