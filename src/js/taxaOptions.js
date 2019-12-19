dc55f3666a50a17c41a22795726fc0f6bde39b4d
lorenzoc <lorenzoc@iastate.edu>
2019-12-19 15:03:50 -0600

    For taxa tab, can now select taxa with checkboxes and they display in the selected taxa table

--- src/js/taxaOptions.js
+++ src/js/taxaOptions.js
@@ -1,13 +1,6 @@
 function createTaxaOptions(){
 
-    //Clears the taxa div
-    // $("#taxaOptions").empty();
-
     //Gets the taxa div
-    var option = document.getElementById("taxaOptions");
-
-    var table = document.getElementById("taxadata");
-
     var tbody = document.getElementById("taxadatatable");
 
     var taxadata = getTaxa();
@@ -21,11 +14,15 @@ function createTaxaOptions(){
             var tr = document.createElement('TR');
             //Td that is appended to Tr
             //Checks for each option
+            var check = $("<input type=\"checkbox\" class=\"custom-control-input\" />");
+            //Sets the function to call for each checkbox on change
+            check[0].setAttribute("onchange","updateSelectTable()" );
             var td = document.createElement('TD');
-            //Taxa name
+            td.appendChild(check[0]);
+            //Taxa names
             var td2 = document.createElement('TD');
             td2.appendChild(document.createTextNode(taxadata[i]));
-            td.style = "width: 20%";
+            td.style = "text-align: center;";
             tr.append(td);
             tr.append(td2);
 
@@ -35,6 +32,51 @@ function createTaxaOptions(){
 
     }
      
+}
+
+function updateSelectTable(){
+    var selectedTaxa = [];
+    var table = document.getElementById("taxadata");
+    for (var i = 1, row; row = table.rows[i]; i++) {
+        var col = row.cells[0].children[0]; 
+        if(col.checked){
+            selectedTaxa.push(i-1);
+        }
+    }
+
+    //Updates the selected taca table
+    var tbody = document.getElementById("selectedtaxatable");
+
+    var taxadata = getTaxa();
+
+    if(taxadata.length !== 0){
+        $("#selectedtaxatable").empty();
+        //Adds the taxa to the table
+        for(var i = 0; i < selectedTaxa.length; i++){
+            //Tr that is appended to tbody
+            var tr = document.createElement('TR');
+            //Td that is appended to Tr
+            var td = document.createElement('TD');
+            //Taxa names
+            td.appendChild(document.createTextNode(taxadata[selectedTaxa[i]]));
+            tr.append(td);
+            tbody.append(tr);
+        }
+
+        //If no taxa is selected display message in body of table
+        if(selectedTaxa.length === 0){
+            //Tr that is appended to tbody
+            var tr = document.createElement('TR');
+            //Td that is appended to Tr
+            var td = document.createElement('TD');
+            //Taxa names
+            td.appendChild(document.createTextNode("No data is selected..."));
+            tr.append(td);
+            tbody.append(tr);
+        }
+
+    }
+
 }
 
 createTaxaOptions();
