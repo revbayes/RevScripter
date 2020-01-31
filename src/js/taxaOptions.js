61112e3fb37f87445c474039244d47a7a3370d86
lorenzoc <lorenzoc@iastate.edu>
2020-01-31 12:55:29 -0600

    Have created a selected taxa table and taxa group table
    Can select taxa from checkboxes and updates the selected taxa table. Can also create a group with the selected taxa table and update the created taxa group table.

--- src/js/taxaOptions.js
+++ src/js/taxaOptions.js
@@ -1,133 +1,253 @@
-function createTaxaOptions(){
+//Selected taxa from table
+var selectedTaxa = [];
 
-    //Gets the taxa div
+//Created taxa groups
+var taxaGroups = [];
+
+function createTaxaOptions() {
+
+    //Taxa tab body
     var tbody = document.getElementById("taxadatatable");
 
     var taxadata = getTaxa();
-    console.log("Taxa size: " + taxadata.length);
+    // console.log("Taxa size: " + taxadata.length);
 
-    if(taxadata.length && taxadata.length !== 0){
+    if (taxadata.length && taxadata.length !== 0) {
+
+        //Enables the checkbox for select all taxa
+        $('#selectalltaxacheckbox').attr("checked", false);
+        $('#selectalltaxacheckbox').removeAttr('disabled');
+
         //Enables the search input for taxa table
         $('#taxadatasearch').removeAttr('disabled');
         //Clears taxa table
         $("#taxadatatable").empty();
         //Adds the taxa to the table
-        for(var i = 0; i < taxa.length; i++){
+        for (var i = 0; i < taxa.length; i++) {
             //Tr that is appended to tbody
             var tr = document.createElement('TR');
             //Td that is appended to Tr
             //Checks for each option
-            var check = $("<input type=\"checkbox\" />");
+            var check = $("<input type=\"checkbox\"/>");
             //Sets the function to call for each checkbox on change
-            check[0].setAttribute("onchange","updateSelectTable()" );
+            check[0].setAttribute("onchange", "updateSelectTable()");
             var td = document.createElement('TD');
             td.appendChild(check[0]);
             //Taxa names
             var td2 = document.createElement('TD');
             td2.appendChild(document.createTextNode(taxadata[i]));
             td.style = "text-align: center;";
+            //Td3 
+            var td3 = document.createElement('TD');
             tr.append(td);
             tr.append(td2);
+            tr.append(td3);
             //Appends the tr to the tbody
             tbody.append(tr);
         }
-    }else{
+
+    } else {
+        //Disables the checkbox for select all taxa
+        $('#selectalltaxacheckbox').attr("checked", false);
+        $('#selectalltaxacheckbox').attr({ 'disabled': 'disabled' });
+
         //Disables the search input for taxa table
-        $('#taxadatasearch').attr({'disabled': 'disabled' });
+        $('#taxadatasearch').attr({ 'disabled': 'disabled' });
+
+        //Clears table
         $("#taxadatatable").empty();
         //Tr that is appended to tbody
         var tr = document.createElement('TR');
-        //Td that is appended to Tr
+        //Td1 that is appended to Tr
         var td = document.createElement('TD');
-        //Taxa names
+        //Td2 Taxa names that is appended to Tr
         var td2 = document.createElement('TD');
         td2.appendChild(document.createTextNode("File has no taxa..."));
         td.style = "text-align: center;";
+        //Td3 
+        var td3 = document.createElement('TD');
         tr.append(td);
         tr.append(td2);
+        tr.append(td3);
         //Appends the tr to the tbody
         tbody.append(tr);
     }
 
     //Adds the filter functionality to the search bar for the taxa data table
     var $rows = $('#taxadatatable tr');
-    $('#taxadatasearch').keyup(debounce(function() {
+    $('#taxadatasearch').keyup(debounce(function () {
 
-    var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
-        reg = RegExp(val, 'i'),
-        text;
+        var val = '^(?=.*\\b' + $.trim($(this).val()).split(/\s+/).join('\\b)(?=.*\\b') + ').*$',
+            reg = RegExp(val, 'i'),
+            text;
 
-    $rows.show().filter(function() {
-        text = $(this).text().replace(/\s+/g, ' ');
-        return !reg.test(text);
-    }).hide();
+        $rows.show().filter(function () {
+            text = $(this).text().replace(/\s+/g, ' ');
+            return !reg.test(text);
+        }).hide();
     }, 300));
 
     function debounce(func, wait, immediate) {
-    var timeout;
-    return function() {
-        var context = this,
-        args = arguments;
-        var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
+        var timeout;
+        return function () {
+            var context = this,
+                args = arguments;
+            var later = function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
         };
-        var callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context, args);
     };
-    };
+
+
+    //Updates Select Table
+    updateSelectTable();
 
 }
 
 //Updates the selected taxa table with selected taxa
-function updateSelectTable(){
-    var selectedTaxa = [];
+function updateSelectTable() {
+    //Taxa index of each taxa that is selected and displayed
+    selectedTaxa = [];
+    var taxaData = getTaxa();
+
     var table = document.getElementById("taxadata");
     for (var i = 1, row; row = table.rows[i]; i++) {
-        var col = row.cells[0].children[0]; 
-        if(col.checked){
-            selectedTaxa.push(i-1);
+        var col = row.cells[0].children[0];
+        //Adds the taxa index if checkbox is checked.
+        if (col.checked) {
+            selectedTaxa.push(taxaData[i - 1]);
         }
     }
 
-    //Updates the selected taxa table
+    //Gets the selected taxa table
     var tbody = document.getElementById("selectedtaxatable");
 
-    var taxadata = getTaxa();
+    // var taxadata = getTaxa();
+    $("#selectedtaxatable").empty();
 
-    if(taxadata.length !== 0){
-        $("#selectedtaxatable").empty();
+    if (selectedTaxa.length !== 0) {
+        //Removes disabled from create taxa group form
+        $('#taxagroupcreate').removeAttr('disabled');
+        $('#taxagroupcreatebutton').removeAttr('disabled');
+
         //Adds the taxa to the table
-        for(var i = 0; i < selectedTaxa.length; i++){
+        for (var i = 0; i < selectedTaxa.length; i++) {
             //Tr that is appended to tbody
             var tr = document.createElement('TR');
             //Td that is appended to Tr
             var td = document.createElement('TD');
             //Taxa names
-            td.appendChild(document.createTextNode(taxadata[selectedTaxa[i]]));
+            td.appendChild(document.createTextNode(selectedTaxa[i]));
             tr.append(td);
             tbody.append(tr);
         }
 
-        //If no taxa is selected display message in body of table
-        if(selectedTaxa.length === 0){
-            //Tr that is appended to tbody
-            var tr = document.createElement('TR');
-            //Td that is appended to Tr
-            var td = document.createElement('TD');
-            //Taxa names
-            td.appendChild(document.createTextNode("No data is selected..."));
-            tr.append(td);
-            tbody.append(tr);
-        }
+    }
 
+    //If no taxa is selected, display message in body of table
+    if (selectedTaxa.length === 0) {
+        //Adds disabled to create a taxa group form
+        $('#taxagroupcreate').attr({ 'disabled': 'disabled' });
+        $('#taxagroupcreatebutton').attr({ 'disabled': 'disabled' });
+
+
+        //Tr that is appended to tbody
+        var tr = document.createElement('TR');
+        //Td that is appended to Tr
+        var td = document.createElement('TD');
+        //Taxa names
+        td.appendChild(document.createTextNode("No data is selected..."));
+        tr.append(td);
+        tbody.append(tr);
     }
 
 }
 
+function updateSelectAllTaxa() {
+    var checktaxa = false;
+
+    if ($('#selectalltaxacheckbox').is(':checked')) {
+        checktaxa = true;
+    }
+
+    //Sets each taxa depending if the select all check box is checked
+    var table = document.getElementById("taxadata");
+    //Loop for checking all checkboxes
+    for (var i = 1, row; row = table.rows[i]; i++) {
+        //Checks the checkbox if it is being displayed
+        if (row.style.display !== "none") {
+            var col = row.cells[0].children[0];
+            col.checked = checktaxa;
+        }
+    }
+
+    //Updates Select table
+    updateSelectTable();
+}
+
+function createTaxaGroup() {
+    //TODO
+    var taxagroup = { name: $("#taxagroupcreate").val(), taxa: selectedTaxa }
+    taxaGroups.push(taxagroup);
+
+    console.log("Group Name: " + taxaGroups[0].name);
+    console.log("taxa: ")
+    for (var i = 0; i < taxaGroups[0].taxa.length; i++) {
+        console.log(taxaGroups[0].taxa[i]);
+    }
+
+    //clears the  taxa group input name
+    $("#taxagroupcreate").val('');
+
+    //Updates Taxa Group Table
+    updateTaxaGroupTable();
+
+}
+
+function updateTaxaGroupTable() {
+    //TODO
+
+    //Gets the selected taxa table
+    var tbody = document.getElementById("taxagrouptable");
+
+    // Clears group taxa table
+    $("#taxagrouptable").empty();
+
+    if (taxaGroups.length !== 0) {
+
+        //Adds the taxa to the table
+        for (var i = 0; i < taxaGroups.length; i++) {
+            //Tr that is appended to tbody
+            var tr = document.createElement('TR');
+            //Td that is appended to Tr
+            var td = document.createElement('TD');
+            //Taxa names
+            td.appendChild(document.createTextNode(taxaGroups[i].name));
+            tr.append(td);
+            tbody.append(tr);
+        }
+
+    }
+
+    //If no taxa is selected, display message in body of table
+    if (taxaGroups.length === 0) {
+        //Tr that is appended to tbody
+        var tr = document.createElement('TR');
+        //Td that is appended to Tr
+        var td = document.createElement('TD');
+        //Taxa names
+        td.appendChild(document.createTextNode("No group is created..."));
+        tr.append(td);
+        tbody.append(tr);
+    }
+}
+
 //Updates the data displayer
-function updateDataDisplayer(){
+function updateDataDisplayer() {
     //TODO
 }
